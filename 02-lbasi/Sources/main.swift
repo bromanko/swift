9a482b6917ccb195e380979668e377cc5d83d59b
bromanko <brian+github@meetearnest.com>
2016-04-03 09:13:29 -0700

    Adding Token class

--- 02-lbasi/Sources/main.swift
+++ 02-lbasi/Sources/main.swift
@@ -1,3 +1,24 @@
 import Foundation
 
-print("Hello, World!")
+enum TokenType {
+    case Integer
+    case Plus
+    case EOF
+}
+
+class Token: CustomStringConvertible {
+    private let type: TokenType;
+    private let value: AnyObject;
+
+    init(type: TokenType, value: AnyObject) {
+        self.type = type;
+        self.value = value;
+    }
+
+    var description: String {
+        return "Token(\(self.type), \(self.value))"
+    }
+}
+
+let token = Token(type: TokenType.Integer, value: 2)
+print(token)
