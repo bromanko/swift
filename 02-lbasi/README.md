97dcab89987d4485989aa21206b363ea30c34ea6
bromanko <brian+github@meetearnest.com>
2016-04-04 11:25:36 -0700

    Adding Dockerfile for compile

---
+++ 02-lbasi/README.md
@@ -0,0 +1,4 @@
+# Let's Build a Simple Interpreter
+
+Swift implementation of Ruslan Spivak's [Let's Build a Simple Interpreter](https://ruslanspivak.com/lsbasi-part1/) blog
+ series.
--- 02-lbasi/Sources/main.swift
+++ 02-lbasi/Sources/main.swift
@@ -7,12 +7,12 @@ enum TokenType {
 }
 
 class Token: CustomStringConvertible {
-    private let type: TokenType;
-    private let value: AnyObject;
+    private let type: TokenType
+    private let value: AnyObject?
 
-    init(type: TokenType, value: AnyObject) {
-        self.type = type;
-        self.value = value;
+    init(type: TokenType, value: AnyObject? = nil) {
+        self.type = type
+        self.value = value
     }
 
     var description: String {
@@ -20,5 +20,39 @@ class Token: CustomStringConvertible {
     }
 }
 
-let token = Token(type: TokenType.Integer, value: 2)
-print(token)
+enum InterpreterError: ErrorProtocol {
+    case CannotParse
+}
+
+class Interpreter {
+    private let text: String
+    private var pos: String.Index
+    private var currentToken: Token? = nil
+
+    init(text: String) {
+        self.text = text
+        self.pos = self.text.characters.startIndex
+    }
+
+    private func getNextToken() throws -> Token {
+        let text = self.text
+
+//        if (self.pos.successor() > text.characters.count - 1) {
+//            return Token(type: TokenType.EOF)
+//        }
+
+        let currentChar = text[self.pos]
+
+        throw InterpreterError.CannotParse
+    }
+
+    func expr() -> String {
+        return "hi"
+    }
+}
+
+
+let response = readLine(strippingNewline: true)
+let interpreter = Interpreter(text: response!)
+let result = interpreter.expr()
+print(result)
